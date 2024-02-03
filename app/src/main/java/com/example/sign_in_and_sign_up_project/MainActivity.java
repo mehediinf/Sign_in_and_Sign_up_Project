8c15b7df984c3f4b8bb819b632086c9e96a6ff69
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-02-04 03:27:33 +0600

    First Commit Sign_in_and_Sign_up_Project

---
+++ app/src/main/java/com/example/sign_in_and_sign_up_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sign_in_and_sign_up_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sign_in_and_Sign_up_Project"
+include(":app")
+ 
